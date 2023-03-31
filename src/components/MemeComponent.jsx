874624984dd5b99acb5f4ca71fc77a0f1bbd3c80
Dhruv Vashist <dhruv.vashist@simpplr.com>
2023-03-31 17:00:37 +0530

    Random images getting displayed on the screen now

--- src/components/MemeComponent.jsx
+++ src/components/MemeComponent.jsx
@@ -1,22 +1,31 @@
-import '../style.css';
-import { useState } from 'react';
-function MemeComp() {
-  const [count, setCount] = useState(() => {
-    console.log('Run USE STATE');
-    return 0;
-  });
-  const handleClick = () => {
-    setCount(count + 1);
-  };
+import React, { useState } from 'react';
+import memesData from '../assets/memeData';
+import '../style.css'
+
+export default function Meme() {
+  // console.log(randMeme.url);
+
+  const [img, setImg] = useState('');
+
+
+  function getImg() {
+    const meme = memesData.data.memes;
+    const randMeme = meme[Math.floor(Math.random() * meme.length)].url;
+    setImg(randMeme)
+  }
 
   return (
-    <main className="form">
-      <input type="text" placeholder="Top Text" className="form--input" />
-      <input type="text" placeholder="Bottom Text" className="form--input" />
-      <button className="form--button" onClick={handleClick}>
-        Get A New Meme 😂 {count}
-      </button>
+    <main>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button" onClick={getImg}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <div className="memeImage">
+        <img src={img} alt="" className="meme--image" />
+      </div>
     </main>
   );
 }
-export default MemeComp;
